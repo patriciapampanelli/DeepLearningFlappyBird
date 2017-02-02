da13414411efd14711de0c4a816081d0eebf4096
Patrícia Pampanelli <patricia.pampanelli@gmail.com>
2017-02-02 13:09:47 -0200

    Tests

--- deep_q_network.py
+++ deep_q_network.py
@@ -13,13 +13,14 @@ from collections import deque
 GAME = 'bird' # the name of the game being played for log files
 ACTIONS = 2 # number of valid actions
 GAMMA = 0.99 # decay rate of past observations
-OBSERVE = 100000. # timesteps to observe before training
-EXPLORE = 2000000. # frames over which to anneal epsilon
+OBSERVE = 10000. # timesteps to observe before training
+EXPLORE = 3000000. # frames over which to anneal epsilon
 FINAL_EPSILON = 0.0001 # final value of epsilon
-INITIAL_EPSILON = 0.0001 # starting value of epsilon
+INITIAL_EPSILON = 0.1 # starting value of epsilon
 REPLAY_MEMORY = 50000 # number of previous transitions to remember
 BATCH = 32 # size of minibatch
 FRAME_PER_ACTION = 1
+
 
 def weight_variable(shape):
     initial = tf.truncated_normal(shape, stddev = 0.01)
